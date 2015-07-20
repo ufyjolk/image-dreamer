d7a75abc75692f62d587cd4bd1a6fb7c5b5ec5f1
Bartosz Makuch <ufyjolk@o2.pl>
2015-07-20 19:31:39 +0200

    dreamify.py script for the 5th test
    Signed-off-by: Bartosz Makuch <ufyjolk@o2.pl>

--- dreamify.py
+++ dreamify.py
@@ -59,7 +59,7 @@ def make_step(net, step_size=1.5, end='inception_4c/output', jitter=32, clip=Tru
         bias = net.transformer.mean['data']
         src.data[:] = np.clip(src.data, -bias, 255-bias)
 
-def deepdream(net, base_img, iter_n=15, octave_n=8, octave_scale=1.4, end='inception_3a/1x1', clip=True, **step_params): #"inter_n=10, octave_n=4, octave_scale=1.4, end='inception_4c/output" was in the original
+def deepdream(net, base_img, iter_n=15, octave_n=8, octave_scale=1.4, end='inception_3a/3x3', clip=True, **step_params): #"end='inception_3a/1x1" was in the last version
     # prepare base images for all octaves
     octaves = [preprocess(net, base_img)]
     for i in xrange(octave_n-1):
